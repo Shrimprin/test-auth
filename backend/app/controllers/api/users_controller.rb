7a1b926678226389044337111fdfa747d853a356
Kuroneko <ken.aster8@gmail.com>
2025-01-04 14:13:38 +0900

    認証した情報をバックエンドでDBに登録できるようにした

---
+++ backend/app/controllers/api/users_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Api
+  class UsersController < ApplicationController
+    def create
+      # 引数の条件に該当するuserを返す。存在しないなら作成する
+      user = User.find_or_create_by!(user_params)
+      if user
+        head :ok
+      else
+        render json: { error: 'ログインに失敗しました' }, status: unprocessable_entity
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+
+    # destoryは退会処理を作るなら必要
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+  end
+end
--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import axios from "axios";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
@@ -9,4 +10,24 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     }),
   ],
   basePath: "/api/auth",
+  callbacks: {
+    // サインインの後に呼ばれる
+    async signIn({ user }) {
+      console.log(user);
+      const name = user.name;
+      const email = user.email;
+      const url = `${process.env.BACKEND_URL}/api/auth/callback/github`;
+
+      try {
+        const response = await axios.post(url, { name, email });
+        if (response.status === 200) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });

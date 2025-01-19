c0352a6ea50d7800eb666f5debeb90353fdafe08
Kuroneko <ken.aster8@gmail.com>
2025-01-19 16:01:39 +0900

    fetcherのファイル名を修正、トークンを含めるように修正

---
+++ frontend/src/utils/fetcher.ts
@@ -0,0 +1,10 @@
+import { AxiosResponse, AxiosError } from "axios";
+import { axiosGet } from "../lib/axios";
+
+export const fetcher = (url: string, token: string | undefined) =>
+  axiosGet(`${url}`, token)
+    .then((res: AxiosResponse) => res.data)
+    .catch((err: AxiosError) => {
+      console.log(err.message);
+      throw err;
+    });
